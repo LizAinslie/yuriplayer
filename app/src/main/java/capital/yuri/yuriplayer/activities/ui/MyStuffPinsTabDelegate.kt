adde6251428537220acee2976174d8e8b84d2182
Liz Ainslie <lizzy@lizainslie.dev>
2026-08-17 22:14:17 -0500

    Delegate MyStuffPinsTab to MyStuffPinsHost (reorder + surface cards)

---
+++ app/src/main/java/capital/yuri/yuriplayer/activities/ui/MyStuffPinsTabDelegate.kt
@@ -0,0 +1,35 @@
+package capital.yuri.yuriplayer.activities.ui
+
+import androidx.compose.runtime.Composable
+import capital.yuri.yuriplayer.data.LibraryIndex
+import capital.yuri.yuriplayer.data.Playlist
+import capital.yuri.yuriplayer.data.Song
+import capital.yuri.yuriplayer.data.StuffPin
+
+/**
+ * Drop-in replacement used from [MyStuffScreen].
+ * Prefer calling [MyStuffPinsHost] directly; this exists so a one-line
+ * rename in MyStuffScreen is enough if the private tab is still referenced.
+ */
+@Composable
+fun MyStuffPinsTabPublic(
+    pins: List<StuffPin>,
+    library: LibraryIndex,
+    playlists: List<Playlist>,
+    allSongs: List<Song>,
+    onOpenPin: (StuffPin) -> Unit,
+    onUnpin: (StuffPin) -> Unit,
+    onAddPinSlot: () -> Unit,
+    onPlayAll: () -> Unit
+) {
+    MyStuffPinsHost(
+        pins = pins,
+        library = library,
+        playlists = playlists,
+        allSongs = allSongs,
+        onOpenPin = onOpenPin,
+        onUnpin = onUnpin,
+        onAddPinSlot = onAddPinSlot,
+        onPlayAll = onPlayAll
+    )
+}
