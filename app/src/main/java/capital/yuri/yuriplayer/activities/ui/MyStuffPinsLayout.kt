c3324e2b07a05ac1ecae7ccacc66a1abcb6c6a56
Liz Ainslie <lizzy@lizainslie.dev>
2026-08-17 21:50:48 -0500

    My Stuff pins: surface cards with inner padding, smaller art, wider gutters, less bottom pad

---
+++ app/src/main/java/capital/yuri/yuriplayer/activities/ui/MyStuffPinsLayout.kt
@@ -0,0 +1,8 @@
+package capital.yuri.yuriplayer.activities.ui
+
+/**
+ * Pin grid layout notes (see MyStuffScreen):
+ * - Cards use surface + 12.dp inner padding
+ * - Art is inset square with text below and spacing
+ * - Grid horizontal padding 20.dp
+ */
