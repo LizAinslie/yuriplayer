f5b2902bd0fdb2aabcea83c51e882258568d096a
Liz Ainslie <lizzy@lizainslie.dev>
2026-08-17 22:46:00 -0500

    Fix accidental bad import in AlbumDetailScreen

--- app/src/main/java/capital/yuri/yuriplayer/activities/ui/AlbumDetailScreen.kt
+++ app/src/main/java/capital/yuri/yuriplayer/activities/ui/AlbumDetailScreen.kt
@@ -48,7 +48,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.Modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawBehind
